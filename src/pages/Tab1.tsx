ff133a8deda667f8c39668100fbf19dcd4e54a79
nguoianphu <nguoianphu@gmail.com>
2021-07-10 10:46:13 +0700

    https://ionicframework.com/docs/utilities/animations#modals

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import ModalExample from '../components/ModalExample';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Tab 1</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ModalExample name="Animation" />
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
